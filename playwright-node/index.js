9277bf7f7d9195781689f4c6c8f73f6bf01474fc
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-23 16:54:11 -0500

    feat: add cors config and add a prefix for the starting the server log

--- playwright-node/index.js
+++ playwright-node/index.js
@@ -2,17 +2,26 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { loggerMiddleware, errorHandlerMiddleware } = require("./middleware");
 const router = require("./routes");
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+const localhost = process.env.LOCALHOST;
+
+const corsConfig = {
+  origin: localhost, // Allowed origins
+  methods: "POST",
+  credentials: false,
+};
 
 // middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(loggerMiddleware);
+app.use(cors(corsConfig));
 
 // Routes
 app.use("/api", router);
@@ -21,5 +30,5 @@ app.use(errorHandlerMiddleware);
 
 // Start the server
 app.listen(PORT, () => {
-  console.log(`Starting server on port ${PORT}...`);
+  console.log(`INFO\tStarting server on port ${PORT}...`);
 });
