456071d77f60c1983de9c87fb5a48f7ed7854413
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-27 10:40:44 -0500

    feat(vite config): add type declarations and use them in tsconfig.node.json

--- playwright-ui/vite-env.d.ts
+++ playwright-ui/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_LOCALHOST: string;
+  VITE_NODE_ENV: string;
+  VITE_ON_RENDER: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
