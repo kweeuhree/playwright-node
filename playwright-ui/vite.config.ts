2b54d8077db2a8df507916423debd792647b0472
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-27 10:39:14 -0500

    feat(vite.config.ts): leverage dotenv and env variables

--- playwright-ui/vite.config.ts
+++ playwright-ui/vite.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const isDevelopment = process.env.VITE_NODE_ENV === "development";
+
+const localhost = process.env.VITE_LOCALHOST;
+const onRender = process.env.VITE_ON_RENDER;
+
+const target = isDevelopment ? localhost : onRender;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: target,
       },
     },
   },
