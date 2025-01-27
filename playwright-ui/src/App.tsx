96c7ccb1cf36004dc197c747f8fc3486c0a15e8f
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-27 13:26:37 -0500

    feat(App): - abstract article display in Article component; - add comments.

--- playwright-ui/src/App.tsx
+++ playwright-ui/src/App.tsx
@@ -5,21 +5,40 @@ import { fetchArticles } from "./api";
 
 import "./App.css";
 
+// Define Article interface
 interface Article {
   title: string;
   url: string;
 }
 
-export const App = () => {
-  const { status, setStatus, statusOptions, setTimedStatus } = useStatus();
-  const [display, setDisplay] = useState<Article[]>([]);
+// Article displays url and title of an article
+const Article = ({ url, title }: Article) => {
+  return (
+    <div key={url} className="white-bd white-hover pd-2 m-02">
+      <p>{title}</p>
+      {/* Open url in a new tab */}
+      <a href={url} target="_blank">
+        {url}
+      </a>
+    </div>
+  );
+};
 
+// Main component
+export const App = () => {
+  // useStatus will provide relevant fetching status, and allow to set the status
+  const { status, setStatus, statusOptions, setTimedStatus } = useStatus();
+  // Set state that will track display of fetched data
+  const [display, setDisplay] = useState<Article[]>([]);
+  // Define a variable that will track whether data for display is fetched
   const fetched = display.length > 0;
 
+  // alreadyFetched() sets a relevant status if data has been already fetched
   const alreadyFetched = () => {
     setTimedStatus(statusOptions.fetched);
   };
 
+  // getArticles() attempts to make a call to the remove server and sets a relevant operation status
   const getArticles = async () => {
     setStatus(statusOptions.loading);
     try {
@@ -33,6 +52,7 @@ export const App = () => {
     }
   };
 
+  // handleClick() determines whether articles should be fetched or no
   const handleClick = () => {
     return fetched ? alreadyFetched() : getArticles();
   };
@@ -42,19 +62,14 @@ export const App = () => {
       <header>
         <h2>Get YCombinator articles!</h2>
       </header>
+      {/* If data has been fetched, change button color to gray */}
       <button className={fetched ? "gray" : ""} onClick={handleClick}>
         Click me
       </button>
+      {/* Display operation status if exists */}
       <div>{status !== "" && status}</div>
-      {display &&
-        display.map((art) => (
-          <div key={art.url} className="white-bd white-hover pd-2 m-02">
-            <p>{art.title}</p>
-            <a href={art.url} target="blank">
-              {art.url}
-            </a>
-          </div>
-        ))}
+      {/* Display an Article component per each object in state */}
+      {display && display.map((article) => <Article {...article} />)}
     </div>
   );
 };
