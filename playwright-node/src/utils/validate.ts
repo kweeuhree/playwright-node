aa1b4f94248b8febb120060af378eb39a20414ba
Nika Kolesnikova <firstnamenika@gmail.com>
2025-01-30 09:53:24 -0500

    fix(validate): correct a mistake

--- playwright-node/src/utils/validate.ts
+++ playwright-node/src/utils/validate.ts
@@ -1,9 +1,11 @@
 export const validateSort = (dateStrings: string[]) => {
+  // If date strings is a single string, it means it holds an error, return
   if (typeof dateStrings === "string") {
-    return dateStrings;
+    return false;
   }
+  // Verify order of dates
   return dateStrings.every((element, index, arr) => {
     if (index === 0) return true;
-    return arr[index] >= element;
+    return arr[index - 1] >= element;
   });
 };
